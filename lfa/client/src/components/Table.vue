29926188537ffd691dd8aebe4864818d03041162
luanraithz <luanraithz@gmail.com>
2019-08-18 20:57:39 -0300

    recreated client

---
+++ lfa/client/src/components/Table.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <div
+            v-for="d in data"
+            >
+            {{ d }}
+        </div>
+    </div>
+</template>
+<script>
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'Table',
+  props: {
+      data: []
+  },
+});
+</script>
+
