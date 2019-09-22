516dca86e3c6ffd4d1692e40467f87ad47874645
luanraithz <luanraithz@gmail.com>
2019-09-22 14:01:44 -0300

    comitando jar (backup)

--- compilers/implementacoes/joyC/src/main/br/com/joyC/impl/lexical/LexicalHttpHandler.java
+++ compilers/implementacoes/joyC/src/main/br/com/joyC/impl/lexical/LexicalHttpHandler.java
@@ -9,7 +9,7 @@ import java.util.Map;
 public class LexicalHttpHandler {
 
     @RequestMapping(name = "Lexical Parser", method = RequestMethod.POST, value="/lexical", consumes = { "application/json" }, produces = { "application/json" })
-    @CrossOrigin(origins = "*")
+    @CrossOrigin(origins = { "http://cmp.luan.raithz.com", "http://localhost:3001" })
     public Map<String, Object> parse(@RequestBody Map<String, String> payload) {
         var content = payload.get("content");
         var result = new HashMap<String, Object>();
